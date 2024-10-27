d10d788a3a948fb80ac7adcd0a6639a4898b35b2
srihari201 <srihariadada19978@gmail.com>
2024-10-27 11:48:56 +0530

    Committing to the SumOfEvenOrOdd program

---
+++ src/day5Practice/SumOfEvenOrOdd.java
@@ -0,0 +1,24 @@
+package day5Practice;
+
+public class SumOfEvenOrOdd {
+	public static void main(String[] args) {
+		
+		
+		 int limit = 10;
+	        int sumEven = 0;
+	        int sumOdd = 0;
+
+	        for (int i = 1; i <= limit; i++) {
+	            if (i % 2 == 0) {
+	                sumEven =sumEven+ i;
+	            } else {
+	                sumOdd = sumOdd+i;
+	            }
+	        }
+	        System.out.println("Sum of even numbers: " + sumEven);
+	        System.out.println("Sum of odd numbers: " + sumOdd);
+	    }
+	}
+	
+
+
