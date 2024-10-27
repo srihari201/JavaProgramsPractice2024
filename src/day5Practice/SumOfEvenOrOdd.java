fcdbfd622c13d35d8d48543ac0bdbb0c646c2236
srihari201 <srihariadada19978@gmail.com>
2024-10-27 11:55:19 +0530

    added syso at the starting and ending

--- src/day5Practice/SumOfEvenOrOdd.java
+++ src/day5Practice/SumOfEvenOrOdd.java
@@ -2,23 +2,22 @@ package day5Practice;
 
 public class SumOfEvenOrOdd {
 	public static void main(String[] args) {
-		
-		
-		 int limit = 10;
-	        int sumEven = 0;
-	        int sumOdd = 0;
+		System.out.println("Execution starts from main method");
+		int limit = 10;
+		int sumEven = 0;
+		int sumOdd = 0;
 
-	        for (int i = 1; i <= limit; i++) {
-	            if (i % 2 == 0) {
-	                sumEven =sumEven+ i;
-	            } else {
-	                sumOdd = sumOdd+i;
-	            }
-	        }
-	        System.out.println("Sum of even numbers: " + sumEven);
-	        System.out.println("Sum of odd numbers: " + sumOdd);
-	    }
+		for (int i = 1; i <= limit; i++) {
+			if (i % 2 == 0) {
+				sumEven = sumEven + i;
+			} else {
+				sumOdd = sumOdd + i;
+			}
+		}
+		System.out.println("Sum of even numbers: " + sumEven);
+		System.out.println("Sum of odd numbers: " + sumOdd);
+		System.out.println("Execution ended");
+
 	}
-	
 
-
+}
