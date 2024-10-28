efdc39db1bbb43bb31da2e2015a167d7f97b7d0f
srihari201 <srihariadada19978@gmail.com>
2024-10-28 10:01:29 +0530

    finding EvenOrOdd and sum of EvenOrOdd

---
+++ src/day5Practice/EvenOrdOdd.java
@@ -0,0 +1,26 @@
+
+package day5Practice;
+
+public class EvenOrdOdd {
+	public static void main(String args[]) {
+		int number = 10;
+		int sumEven = 0;
+		int sumOdd = 0;
+		System.out.println("The given number is: " + number);
+		for (int i = 0; i <= number; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + "is even");
+				sumEven += i;
+				System.out.println();
+			} else {
+				System.out.println(i + "is odd");
+				sumOdd += i;
+
+			}
+		}
+		System.out.println("sum of even is: " + sumEven);
+		System.out.println("sum of Odd is: " + sumOdd);
+
+	}
+
+}
